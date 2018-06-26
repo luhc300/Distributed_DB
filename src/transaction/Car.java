1d5a58a1374eb69672b237b8486c02cf0a2251fb
ssilv <18516053895@163.com>
2018-06-26 16:23:44 +0800

    reformat

--- src/transaction/Car.java
+++ src/transaction/Car.java
@@ -4,101 +4,99 @@ import java.io.Serializable;
 
 /**
  * @author junhuihan
- *
  */
-public class Car implements ResourceItem, Serializable{
+public class Car implements ResourceItem, Serializable {
 
-	public static final String INDEX_LOCATION = "location";
-	
-	protected String location;
-	protected int price;
-	protected int numCars;
-	protected int numAvail;
-	
-	protected boolean isdeleted = false;
+    public static final String INDEX_LOCATION = "location";
 
-	public Car(String location, int price, int numCars, int numAvail) {
-		this.location = location;
-		this.price = price;
-		this.numCars = numCars;
-		this.numAvail = numAvail;
-	}
+    protected String location;
+    protected int price;
+    protected int numCars;
+    protected int numAvail;
 
-	public String getLocation() {
-		return location;
-	}
+    protected boolean isdeleted = false;
 
-	public void setLocation(String location) {
-		this.location = location;
-	}
+    public Car(String location, int price, int numCars, int numAvail) {
+        this.location = location;
+        this.price = price;
+        this.numCars = numCars;
+        this.numAvail = numAvail;
+    }
 
-	public int getPrice() {
-		return price;
-	}
+    public String getLocation() {
+        return location;
+    }
 
-	public void setPrice(int price) {
-		this.price = price;
-	}
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
-	public int getNumCars() {
-		return numCars;
-	}
+    public int getPrice() {
+        return price;
+    }
 
-	public void setNumCars(int numCars) {
-		this.numCars = numCars;
-	}
+    public void setPrice(int price) {
+        this.price = price;
+    }
 
-	public int getNumAvail() {
-		return numAvail;
-	}
+    public int getNumCars() {
+        return numCars;
+    }
 
-	public void setNumAvail(int numAvail) {
-		this.numAvail = numAvail;
-	}
+    public void setNumCars(int numCars) {
+        this.numCars = numCars;
+    }
 
-	public String[] getColumnNames() {
-		// TODO Auto-generated method stub
-		return new String[] { "location", "price", "numCars", "numAvail"};
-	}
+    public int getNumAvail() {
+        return numAvail;
+    }
 
-	public String[] getColumnValues() {
-		// TODO Auto-generated method stub
-		return new String[] {location, "" + price, "" + numCars, "" + numAvail};
-	}
+    public void setNumAvail(int numAvail) {
+        this.numAvail = numAvail;
+    }
 
-	public Object getIndex(String indexName) throws InvalidIndexException {
-		// TODO Auto-generated method stub
-		if (indexName.equals(INDEX_LOCATION)) {
-			return location;
-		}else if (indexName.equals("price")){
-			return price;
-		}else if (indexName.equals("numCars")){
-			return numCars;
-		}else if (indexName.equals("numAvail")){
-			return numAvail;
-		}
-		else{
-			throw new InvalidIndexException(indexName);
-		}
-	}
+    public String[] getColumnNames() {
+        // TODO Auto-generated method stub
+        return new String[]{"location", "price", "numCars", "numAvail"};
+    }
 
-	public Object getKey() {
-		return location;
-	}
+    public String[] getColumnValues() {
+        // TODO Auto-generated method stub
+        return new String[]{location, "" + price, "" + numCars, "" + numAvail};
+    }
 
-	public boolean isDeleted() {
+    public Object getIndex(String indexName) throws InvalidIndexException {
+        // TODO Auto-generated method stub
+        if (indexName.equals(INDEX_LOCATION)) {
+            return location;
+        } else if (indexName.equals("price")) {
+            return price;
+        } else if (indexName.equals("numCars")) {
+            return numCars;
+        } else if (indexName.equals("numAvail")) {
+            return numAvail;
+        } else {
+            throw new InvalidIndexException(indexName);
+        }
+    }
 
-		return isdeleted;
-	}
+    public Object getKey() {
+        return location;
+    }
 
-	public void delete() {
+    public boolean isDeleted() {
 
-		isdeleted = true;
-	}
-	
-	public Object clone() {
-		Car c = new Car(getLocation(), getPrice(), getNumCars(), getNumAvail());
-		c.isdeleted = isdeleted;
-		return c;
-	}
+        return isdeleted;
+    }
+
+    public void delete() {
+
+        isdeleted = true;
+    }
+
+    public Object clone() {
+        Car c = new Car(getLocation(), getPrice(), getNumCars(), getNumAvail());
+        c.isdeleted = isdeleted;
+        return c;
+    }
 }
