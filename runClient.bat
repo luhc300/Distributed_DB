9e9b25fd97b8a23bba6b78d9a573204097ffa8e3
DavidJett <594265453@qq.com>
2018-07-30 15:06:06 +0800

    v4.0

--- runClient.bat
+++ runClient.bat
@@ -1,2 +1,2 @@
-java -classpath %cd%\bin -Djava.security.policy=%cd%\src\transaction\security-policy transaction.Client
+java -classpath %cd%\bin -Djava.security.policy=%cd%\src\transaction\security-policy transaction.Client1
 pause
